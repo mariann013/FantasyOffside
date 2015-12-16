b3b33124c5c5a06c5e8b7df357a15156be83ca37
mateja683 <mateja683@gmail.com>
2015-12-16 17:50:50 +0000

    set up heroku to display scraped user team

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'index#index'
 
+  post 'get-squad' => 'index#squadid', as: :squadid
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
