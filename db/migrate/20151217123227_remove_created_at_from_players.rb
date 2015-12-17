efee569f361a04f3c92ec7db3bd5db711a0723b2
thisdotrob <thisdotrob@gmail.com>
2015-12-17 15:03:46 +0000

    Remove duplicate migrations

--- db/migrate/20151217123227_remove_created_at_from_players.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCreatedAtFromPlayers < ActiveRecord::Migration
-  def change
-    remove_column :players, :created_at, :timestamp
-  end
-end
--- db/migrate/20151217123305_remove_updated_at_from_players.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUpdatedAtFromPlayers < ActiveRecord::Migration
-  def change
-    remove_column :players, :updated_at, :timestamp
-  end
-end
