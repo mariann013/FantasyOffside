cb7965f067a93138c78defa30875621a5ec4414c
mateja683 <mateja683@gmail.com>
2015-12-17 17:53:33 +0000

    adding procfile for gameweeks and season totals

--- scrape_files/league_scrapping.py
+++ scrape_files/league_scrapping.py
@@ -4,12 +4,29 @@ import re
 import io
 from bs4 import BeautifulSoup
 import os
+import psycopg2
+import urlparse
+import sys
 
+urlparse.uses_netloc.append("postgres")
 
-#################################################
-# Extract the league table from the website #
-#################################################
-base = "http://fantasy.premierleague.com/transfers/"
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+sys.stdout.write("Scraping players! ")
+sys.stdout.flush()
+
+base = "http://fantasy.premierleague.com/"
 html = urllib.urlopen(base).read()
 soup = BeautifulSoup(html)
 #scrape the website for the games played and points
@@ -22,17 +39,13 @@ for text in soup.find_all('table', {"class":'leagueTable'}):
     for links in text.find_all('a'):
         team.append(links.text.strip())
 
-league_table = open("league_table.txt", "w")
-league_table.close()
-
-league_table = open("league_table.txt", "a")
-#print out the league table
 i=0
 while i < 20:
+    cursor.execute("INSERT INTO league_table VALUES (%s, %s, %s, %s, %s)",
+        (i, teamid, position, price))
     league_table.write(str(i+1) + "," + team[i] + "," + games_played[i].text + "," + points[i].text + "\n")
-
+    sys.stdout.write(".")
+    sys.stdout.flush()
     i +=1
-league_table.close()
 
 print "League table scraped"
-print "All data scraped"
