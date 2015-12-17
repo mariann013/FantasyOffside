f19c14bdacc80c457e2410830daab70378c3f2ba
thisdotrob <thisdotrob@gmail.com>
2015-12-17 16:42:53 +0000

    Lookup scraped squad id's in database, return detailed info in json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'index#index'
 
-  post 'get-squad' => 'index#squadid', as: :squadid
+  post 'getsquad' => 'index#getsquad', as: :getsquad
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
