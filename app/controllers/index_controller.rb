b0d90fca8b38d2a0320174eb02ad3b5503d524b9
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-12-18 16:53:31 +0000

    another validation on api params

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,9 @@ require './lib/modules/squad_scraper.rb'
 class IndexController < ApplicationController
 
   def getsquad
-    if (params[:fplid] =~ /[^0-9]/) == nil
+    if params[:fplid] == ""
+      render json: "Must provide team id", status: 400
+    elsif (params[:fplid] =~ /[^0-9]/) == nil
       render json: SquadScraper.getSquadJSON(params[:fplid]), status: :ok
     else
       render json: "Invalid team id number", status: 400
