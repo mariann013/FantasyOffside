8eb413df97d42c2e0cd19982f7bdc52d1ac43dae
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2016-01-04 14:57:56 +0000

    Goalkeeper projected points implemented

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,23 +4,20 @@ class Player < ActiveRecord::Base
 
 
   def self.update_gk
-    player = Player.find(1)
-    pid = player.id
-
-    if player.position = "Goalkeeper"
-      total_points = []
-      PlayerGameweekTotal.where(playerid: pid).find_each do |player|
-        total_points << player.total_points
+    i = 1
+    while i < 650
+      player = Player.find(i)
+      if player.position = "Goalkeeper"
+        total_points = []
+        PlayerGameweekTotal.where(playerid: player.id).find_each do |player|
+          total_points << player.total_points
+        end
+        average = total_points.inject{ |sum, el| sum + el }.to_i / total_points.size
       end
-      average = total_points.inject{ |sum, el| sum + el }.to_i / total_points.size
-      player.update(projected_points: average)
-
-
-        # total_points = PlayerGameweekTotal.find_each(playerid: pid)
-        # p total_points
-        # total_points = PlayerGameweekTotal.find(player.id).pluck(:total_points)
+      player.projected_points = average
+      player.save
+      i += 1
     end
-    #   i += 1
-    # end
   end
+
 end
--- spec/models/players_spec.rb
+++ spec/models/players_spec.rb
@@ -9,6 +9,12 @@ describe Player, type: :model do
   it 'is expected to output 4 points for projected points' do
     DatabaseCleaner.clean
     player = Player.create(playerdata: "test_player", teamid: 1, position: "Goalkeeper", price: 5.5)
+    i = 1
+    while i <= 650
+      Player.create(playerdata: "test")
+      PlayerGameweekTotal.create(playerid: i, gameweek: '5', total_points: '4')
+      i += 1
+    end
     Team.create(name: "test_team")
     PlayerGameweekTotal.create(playerid: "1", gameweek: '5', total_points: '4')
     PlayerGameweekTotal.create(playerid: "1", gameweek: '4', total_points: '5')
@@ -16,6 +22,7 @@ describe Player, type: :model do
     PlayerGameweekTotal.create(playerid: "1", gameweek: '2', total_points: '4')
     PlayerGameweekTotal.create(playerid: '1', gameweek: '1', total_points: '4')
     Player.update_gk
+    player = Player.find(1)
     expect(player.projected_points).to eq(4)
   end
 end
