2f08187f379941dde7b2ba1388da40bc38367e6d
mateja683 <mateja683@gmail.com>
2016-01-04 14:17:05 +0000

    half finished gk projected points

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,26 @@
+require_relative 'player_gameweek_total'
+
 class Player < ActiveRecord::Base
+
+
+  def self.update_gk
+    player = Player.find(1)
+    pid = player.id
+
+    if player.position = "Goalkeeper"
+      total_points = []
+      PlayerGameweekTotal.where(playerid: pid).find_each do |player|
+        total_points << player.total_points
+      end
+      average = total_points.inject{ |sum, el| sum + el }.to_i / total_points.size
+      player.update(projected_points: average)
+
+
+        # total_points = PlayerGameweekTotal.find_each(playerid: pid)
+        # p total_points
+        # total_points = PlayerGameweekTotal.find(player.id).pluck(:total_points)
+    end
+    #   i += 1
+    # end
+  end
 end
---
+++ spec/models/players_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+
+describe Player, type: :model do
+  it 'is expected to output 4 points for projected points' do
+    DatabaseCleaner.clean
+    player = Player.create(playerdata: "test_player", teamid: 1, position: "Goalkeeper", price: 5.5)
+    Team.create(name: "test_team")
+    PlayerGameweekTotal.create(playerid: "1", gameweek: '5', total_points: '4')
+    PlayerGameweekTotal.create(playerid: "1", gameweek: '4', total_points: '5')
+    PlayerGameweekTotal.create(playerid: "1", gameweek: '3', total_points: '3')
+    PlayerGameweekTotal.create(playerid: "1", gameweek: '2', total_points: '4')
+    PlayerGameweekTotal.create(playerid: '1', gameweek: '1', total_points: '4')
+    Player.update_gk
+    expect(player.projected_points).to eq(4)
+  end
+end
