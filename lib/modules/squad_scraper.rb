c198e8904037cfd69a8e04cea7ecebfba2fb0cfc
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2016-01-06 19:05:26 +0000

    getsquad method returns output in correct json format

--- lib/modules/squad_scraper.rb
+++ lib/modules/squad_scraper.rb
@@ -4,8 +4,19 @@ module SquadScraper
 
   def self.getSquadJSON(fplid)
     squad_ids = scrapeSquad(fplid)
-    squadJSON = lookupSquadFromIds(squad_ids)
+    squadJSON = makeSquadArrayByPosition(squad_ids)
+    return {
+      squad: squadJSON,
+      playerids: squad_ids,
+      # formation: "",
+      # captain: "",
+      # vicecaptain: "",
+      cash: 5.2
+    }
+
   end
+
+  private
 
   def self.scrapeSquad(fplid)
     squad = []
@@ -15,19 +26,48 @@ module SquadScraper
     rows = browser.table(:id, "ismTeamDisplayData").rows
     for i in 1..(rows.length-1)
       link_str = rows[i].cells[1].link(:class, 'ismInfo ismViewProfile JS_ISM_INFO').href
-      squad << link_str.split("#").last
+      squad << link_str.split("#").last.to_i
     end
     browser.close
     squad
   end
 
-  def self.lookupSquadFromIds(squad_ids)
-    squadJson = squad_ids.map do |id|
-      player = Player.find(id)
-      team = Team.find(player.teamid)
-      player
+
+
+  def self.makeSquadArrayByPosition(squad_ids)
+    squad_with_positions = {
+      Goalkeeper: [],
+      Defender: [],
+      Midfielder: [],
+      Forward: [],
+    }
+    new_squad = squad_ids.map { |playerid| Player.find(playerid) }
+    newer_squad = new_squad[0,11]
+    subs = new_squad[11,4]
+    newer_squad.each do |player|
+      player = PlayerFormatter.format(player.id)
+      squad_with_positions[player[:position].to_sym] << player
     end
-    squadJson
+    lineup = {
+      goalkeeper: nil,
+      defenders: [],
+      midfielders: [],
+      forwards: [],
+      substitutes: []
+    }
+    lineup[:goalkeeper] = squad_with_positions[:Goalkeeper].pop
+    noOfDefenders = squad_with_positions[:Defender].count
+    noOfDefenders.times { lineup[:defenders] << squad_with_positions[:Defender].shift }
+    noOfMidfielders = squad_with_positions[:Midfielder].count
+    noOfMidfielders.times { lineup[:midfielders] << squad_with_positions[:Midfielder].shift }
+    noOfForwards = squad_with_positions[:Forward].count
+    noOfForwards.times { lineup[:forwards] << squad_with_positions[:Forward].shift }
+
+    subs.each do |player|
+      player = PlayerFormatter.format(player.id)
+      lineup[:substitutes] << player
+    end
+    lineup
   end
 
 end
