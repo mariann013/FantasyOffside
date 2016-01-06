a30dbe30121ddffb9f3823a3d71167b63f26de09
thisdotrob <thisdotrob@gmail.com>
2016-01-06 20:15:10 +0000

    formation displayed on optimised squad page

--- public/js/fOffsideController.js
+++ public/js/fOffsideController.js
@@ -4,8 +4,8 @@ fOffside.controller('FOffsideController', ['$http', function($http) {
 
   self.init = function() {
     self.showForm = true;
-    self.showTable = false;
-    self.showOptimiseButton = false;
+    self.displayingCurrentSquad = false;
+    self.displayingOptimisedSquad = false;
   };
 
 
@@ -15,17 +15,11 @@ fOffside.controller('FOffsideController', ['$http', function($http) {
       method: 'GET',
       params: {fplid: self.inputId}
     }).then(function(res) {
-      self.goalkeeper = res.data.squad.goalkeeper;
-      self.defenders = res.data.squad.defenders;
-      self.midfielders = res.data.squad.midfielders;
-      self.forwards = res.data.squad.forwards;
-      self.substitutes = res.data.squad.substitutes;
       self.showForm = false;
-      self.showTable = true;
-      console.log(res.data.playerids);
+      self.displayingCurrentSquad = true;
+      self.parsePlayers(res.data);
       self.playerIds = res.data.playerids;
       self.cash = res.data.cash;
-      self.showOptimiseButton = true;
     });
   };
 
@@ -35,14 +29,18 @@ fOffside.controller('FOffsideController', ['$http', function($http) {
       method: 'GET',
       params: {squad: JSON.stringify(self.playerIds), cash: self.cash }
     }).then(function(res) {
-      self.goalkeeper = res.data.squad.goalkeeper;
-      self.defenders = res.data.squad.defenders;
-      self.midfielders = res.data.squad.midfielders;
-      self.forwards = res.data.squad.forwards;
-      self.substitutes = res.data.squad.substitutes;
-      self.cash = res.data.cash;
-      self.showOptimiseButton = false;
+      self.displayingCurrentSquad = false;
+      self.displayingOptimisedSquad = true;
+      self.parsePlayers(res.data);
     });
+  };
+
+  self.parsePlayers = function(data) {
+    self.goalkeeper = data.squad.goalkeeper;
+    self.defenders = data.squad.defenders;
+    self.midfielders = data.squad.midfielders;
+    self.forwards = data.squad.forwards;
+    self.substitutes = data.squad.substitutes;
   };
 
   self.init();
