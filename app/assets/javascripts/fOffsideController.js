23833b8732280512635422fe91ec87904a153a48
mateja683 <mateja683@gmail.com>
2016-01-07 16:49:36 +0000

    initial suqad formation

--- app/assets/javascripts/fOffsideController.js
+++ app/assets/javascripts/fOffsideController.js
@@ -31,17 +31,35 @@ fOffside.controller('FOffsideController', ['$http', function($http) {
     }).then(function(res) {
       self.displayingCurrentSquad = false;
       self.displayingOptimisedSquad = true;
+      self.parseCaptains(res.data);
+      self.parseTransfers(res.data);
       self.parsePlayers(res.data);
+      self.cash = res.data.cash;
+      console.log(res.data);
     });
   };
 
   self.parsePlayers = function(data) {
     self.goalkeeper = data.squad.goalkeeper;
     self.defenders = data.squad.defenders;
+    self.defenders.pop();
     self.midfielders = data.squad.midfielders;
+    self.midfielders.push(self.defenders[0]);
     self.forwards = data.squad.forwards;
     self.substitutes = data.squad.substitutes;
   };
+
+  self.parseCaptains = function(data) {
+    self.captain = data.captain;
+    self.viceCaptain = data.vicecaptain;
+  };
+
+  self.parseTransfers = function(data) {
+    self.transferOut = data.transfers.out;
+    self.transferIn = data.transfers.in;
+
+  };
+
 
   self.init();
 
