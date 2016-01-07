7995f1571f9474b44a89ed9fd15ff753fcf5214d
mateja683 <mateja683@gmail.com>
2016-01-07 18:25:00 +0000

    added pitch and arrows

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module FantasyOffside
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
 
+
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
